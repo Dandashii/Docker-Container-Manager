9e0630ee3d253d3502ece19b5f096207d8ab7e47
Mohammad Dandashi <m7amad_00@hotmail.com>
2023-03-18 23:04:39 +0200

    Implemented a pageable workers response

--- openfabric-api/src/main/java/ai/openfabric/api/repository/WorkerRepository.java
+++ openfabric-api/src/main/java/ai/openfabric/api/repository/WorkerRepository.java
@@ -1,10 +1,13 @@
 package ai.openfabric.api.repository;
 
 import ai.openfabric.api.model.Worker;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 
 public interface WorkerRepository extends CrudRepository<Worker, String> {
-    Optional<Worker> findById();
+    Optional<Worker> findById(String id);
+    Page<Worker> findAll(Pageable pageable);
 }
