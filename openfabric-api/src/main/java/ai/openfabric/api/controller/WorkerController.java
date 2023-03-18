1e01f6d462d6d5f63f24c5ab01d5afef775d17e6
Mohammad Dandashi <m7amad_00@hotmail.com>
2023-03-18 23:05:14 +0200

    defined the retrieval of all available workers.

--- openfabric-api/src/main/java/ai/openfabric/api/controller/WorkerController.java
+++ openfabric-api/src/main/java/ai/openfabric/api/controller/WorkerController.java
@@ -1,13 +1,24 @@
 package ai.openfabric.api.controller;
 
+import ai.openfabric.api.model.Worker;
+import ai.openfabric.api.repository.WorkerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("${node.api.path}/worker")
 public class WorkerController {
 
-    @PostMapping(path = "/hello")
-    public @ResponseBody String hello(@RequestBody String name) {
-        return "Hello!" + name;
+    private WorkerRepository repository;
+
+    @GetMapping("/getWorkers")
+    public Page<Worker> getWorkers(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "3") int size) {
+        return repository.findAll(PageRequest.of(page, size));
     }
 }
