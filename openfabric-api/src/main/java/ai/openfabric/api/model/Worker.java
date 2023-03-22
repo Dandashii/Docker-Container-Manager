a31932693c2f14accb50b85d1827ade025469503
Mohammad Dandashi <m7amad_00@hotmail.com>
2023-03-22 11:05:09 +0200

    Added status property

--- openfabric-api/src/main/java/ai/openfabric/api/model/Worker.java
+++ openfabric-api/src/main/java/ai/openfabric/api/model/Worker.java
@@ -5,20 +5,24 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Objects;
 
+
+@Getter
+@Setter
 @Entity()
+@Table(name = "worker")
 public class Worker extends Datable implements Serializable {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "of-uuid")
     @GenericGenerator(name = "of-uuid", strategy = "ai.openfabric.api.model.IDGenerator")
-    @Getter
-    @Setter
     public String id;
     public String name;
+    public String status;
+
+    public boolean isRunning() {
+        return Objects.equals(this.status, "running");
+    }
 }
